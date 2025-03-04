c192f3226881c921e1e9beb90199cc5cc6c8b748
Parth200611 <parthdahapute7@gmail.com>
2025-03-04 22:50:30 +0530

    commit

---
+++ app/src/main/java/com/mountreachsolution/vibez/urls.java
@@ -0,0 +1,7 @@
+package com.mountreachsolution.vibez;
+
+public class urls {
+    public static String address="http://192.168.1.3:80/vibez/";
+    public static String userregister=address+"userregister.php";
+    public static String login=address+"login.php";
+}
